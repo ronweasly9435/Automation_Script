ef493589e630e0981d41fc7c25f6c3e60054d218
Mazumder <roniemaz@amazon.com>
2025-05-03 09:49:14 +0530

    fix chromium 7

--- server.js
+++ server.js
@@ -341,7 +341,16 @@ async function scrapeAllProducts() {
   
   const browser = await puppeteer.launch({ 
     headless: true,
-  args: ['--no-sandbox', '--disable-setuid-sandbox'], 
+  args: [
+    '--no-sandbox', 
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu'
+  ], 
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath() 
   });
 
